aa5869c8f537df59cab9c4be80260462a17c3657
ktkhushi2006 <ktkhushi916@example.com>
2026-05-10 01:08:44 +0530

    render deployment setup

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-1234567890-your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,5 +62,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'resume_app', 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
